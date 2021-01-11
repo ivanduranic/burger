51a62d314c43382d3e9ec172e7f5c93711386aad
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-10 21:44:26 -0500

    created db folder with schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Holy Chuck Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The County General Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Golden Star Burger', true);
