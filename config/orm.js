a2c7e4ca690df033dfa81e3aa6ffe323af3141aa
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-12 22:01:16 -0500

    wrote selectAll method

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,16 @@
 const connection = require("./connection.js");
-// const orm = {
-//   selectAll: function (tableInput, colToSearch, valOfCol) {
-//     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-
-//     connection.query(
-//       queryString,
-//       [tableInput, colToSearch, valOfCol],
-//       function (err, result) {
-//         if (err) throw err;
-//         return result;
-//         console.log(result);
-//       }
-//     );
-//   },
+const orm = {
+  selectAll: function (tableInput, cb) {
+var queryString = "SELECT * FROM ??";
+    connection.query(
+      queryString,
+      tableInput,
+      function (err, result) {
+        if (err) throw err;
+        return result;
+      }
+    );
+  },
 //   insertOne: function (tableInput, colToSearch, valOfCol) {
 //     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
 
