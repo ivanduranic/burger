1ed4edccfe58e847b74e2881f633f09cc79df216
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-13 22:22:47 -0500

    working on methods

--- config/orm.js
+++ config/orm.js
@@ -1,41 +1,32 @@
 const connection = require("./connection.js");
 const orm = {
   selectAll: function (tableInput, cb) {
-var queryString = "SELECT * FROM ??";
-    connection.query(
-      queryString,
-      tableInput,
-      function (err, result) {
-        if (err) throw err;
-        return result;
-      }
-    );
+    let queryString = "SELECT * FROM ??";
+    connection.query(queryString, tableInput, function (err, result) {
+      if (err) throw err;
+      return result;
+    });
   },
-//   insertOne: function (tableInput, colToSearch, valOfCol) {
-//     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
 
-//     connection.query(
-//       queryString,
-//       [tableInput, colToSearch, valOfCol],
-//       function (err, result) {
-//         if (err) throw err;
-//         return result;
-//         console.log(result);
-//       }
-//     );
-//   },
-//   updateOne: function (tableInput, colToSearch, valOfCol) {
-//     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-//     connection.query(
-//       queryString,
-//       [tableInput, colToSearch, valOfCol],
-//       function (err, result) {
-//         if (err) throw err;
-//         return result;
-//         console.log(result);
-//       }
-//     );
-//   },
-// };
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += createQmarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
 module.exports = orm;
