1d186c0c3b08994e1784ccd4a768dd0d005ac7b7
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-14 00:28:19 -0500

    connection with Heroku done

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: "",
+  host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "p7eqjty679jpi7vh",
+  password: "usjinyo450rrbnlt",
+  database: "fzda62k2fpxipir5",
 });
 
 connection.connect(function (err) {
