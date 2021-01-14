30c456c45cc19e6ae251f2896070aed1de0b1f2f
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-13 22:35:16 -0500

    created routers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js");
+// Get router
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    let hdlbrsObj = {
+      burgers: data,
+    };
+
+    res.render("index", hdlbrsObj);
+  });
+});
+
+//Post router
+router.post("/api/burger", function (req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, false],
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+//Put router
+router.put("/api/burger/:id", function (req, res) {
+  let id = req.params.id;
+  burger.updateOne(id, function (result) {});
+});
 
 module.exports = router;
